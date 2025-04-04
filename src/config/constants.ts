433492e7024653b3c2dd5bfb0a108eae0d1c37d1
Andrés Salgado <andressalgado41@gmail.com>
2025-04-03 23:29:08 -0500

    Add const

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,6 +6,7 @@ dotenv.config();
 export const NODE_ENV = process.env.NODE_ENV || 'dev';
 export const PORT = Number(process.env.APP_PORT) || 4000;
 export const RETELL_API_KEY_3MT = process.env.RETELL_API_KEY_3MT || 'agregarelaclave';
+export const RETELL_API_KEY_WSC = process.env.RETELL_API_KEY_WSC || 'agregarelaclave';
 export const HOSTNAME = process.env.HOSTNAME || `http://localhost:${PORT}`;
 export const HOSTWEB = process.env.HOSTWEB || 'http://127.0.0.1:5173';
 export const TZ = process.env.TZ || 'America/Guayaquil';
