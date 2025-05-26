7beb3355b76708ef8d3ca0942750250785051a83
Andrés Salgado <andressalgado41@gmail.com>
2025-05-26 17:58:47 -0500

    Feat: Add rate limiter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,18 @@
 import {Router, Request, Response} from 'express';
 import {errorMiddleware} from '../middleware/errorMiddleware';
-import { appRouter } from './app';
+import {appRouter} from './app';
+
+import {rateLimit} from 'express-rate-limit';
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 5,
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+});
 
 export const router = Router();
 
-router.use('/aiconnect', appRouter);
+router.use('/aiconnect', limiter, appRouter);
 
 router.use(errorMiddleware);
 router.use((req: Request, res: Response) => {
